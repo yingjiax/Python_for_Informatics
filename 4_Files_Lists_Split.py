97110a2cd363819b5fb9f251e86b860f3e768f54
mGalarnyk <mgalarny@gmail.com>
2016-02-07 22:49:00 -0800

    assignment 4

--- 4_Files_Lists_Split.py
+++ 4_Files_Lists_Split.py
@@ -3,3 +3,23 @@
 Michael Galarnyk
 Assignment: Files, Lists, and Split
 """
+
+def freq_count(user_string, unique_words):
+    for word in unique_words:
+        print word, ":", user_string.count(word);
+        
+text_file_name = raw_input('Enter a text file please \n');
+user_string = raw_input('What substring do you want ? \n')
+with open('romeo.txt', 'r') as f:
+    script_list = [];
+    line_list = [];
+    for line in f:
+        line_list.extend(line.split());
+
+for word in line_list:
+    if word not in script_list:
+        script_list.append(word);
+         
+script_list.sort();
+
+freq_count(user_string, script_list);
